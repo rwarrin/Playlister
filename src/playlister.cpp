525db1c704d6cb231efd920830b7a7e936104ae9
Rick Warrington <phial1020@gmail.com>
2012-06-21 11:11:34 -0600

    Add status text to show progress for large batches.
    Add a message "Processing x of x." so a user knows something is happening
    when large batches are being processed.

--- src/playlister.cpp
+++ src/playlister.cpp
@@ -11,14 +11,18 @@
 
 #include <iostream>
 #include <string>
+#include <stdio.h>  // for fflush() and stdout
 
 #include "../include/playlist_builder.h"
 
 int main(int argc, char *argv[]) {
     using namespace std;
 
-    for(int i = 1; i < argc; i++)
+    for(int i = 1; i < argc; i++) {
+        cout << "\rProcessing " << i << " of " << argc - 1 << ".";
+        fflush(stdout);
         BuildPlaylist(argv[i]);
+    }
 
     return 0;
 }
